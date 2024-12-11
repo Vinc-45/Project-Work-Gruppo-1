5bb3f8bd14cb563de1c391829974e42b9dba1735
chiaragambardella <>
2024-12-11 18:01:08 +0100

    Aggiunta del metodo verify di VerificaTelefono e casi di test

--- src/main/java/it/unisa/diem/softeng/gruppo1/VerificaTelefono.java
+++ src/main/java/it/unisa/diem/softeng/gruppo1/VerificaTelefono.java
@@ -1,20 +1,28 @@
-
 package it.unisa.diem.softeng.gruppo1;
-
 /**
  * @brief Classe per la verifica della validità del numero di telefono
  */
-public class VerificaTelefono implements Verifica {
 
-    /**
+public class VerificaTelefono implements Verifica {
+    
+    
+     /**
      * @brief Controlla se l'attributo numero di telefono è valido
      * @param[in] contact Il contatto di cui si vuole verificare la validità
      * @return true se il numero di telefono è valido altrimenti false
     */
     @Override
-    public boolean verify(String[] s) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public boolean verify(String[] num) {
+        if(num[0].isEmpty() && num[1].isEmpty() && num[2].isEmpty()) return true;//verifico se tutte e tre le stringhe sono vuote
+        for(int i=0;i<num.length;i++){
+            if(!num[i].isEmpty()){
+            String cleanNum=num[i].replaceAll(" ", "");//tolgo gli eventuali spazi all'interno delle stringhe
+            System.out.println(cleanNum);
+            for(int j=0;j<cleanNum.length();j++)
+            if(!cleanNum.matches("[0-9]+")) return false;//verifico che all'interno della stringa siano presenti solo numeri
+            }
+        }
+        
+        return true;
     }
-    
-    
 }
---
+++ src/test/java/it/unisa/diem/softeng/gruppo1/VerificaTelefonoTest.java
@@ -0,0 +1,85 @@
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+import prova.tryrubricamaven.Contatto;
+import prova.tryrubricamaven.VerificaTelefono;
+
+public class VerificaTelefonoTest {
+    
+    @Test
+    public void testVerify(){
+        
+        String[] num = {"12649875", "65891472", "32864791"};
+        VerificaTelefono vt = new VerificaTelefono();
+        assertTrue(vt.verify(num));
+        
+    }
+    
+    @Test
+    public void testVerify2(){
+        
+        String[] num = {"85473294", "45971234", ""};
+        VerificaTelefono vt = new VerificaTelefono();
+        assertTrue(vt.verify(num));
+        
+    }
+    
+    @Test
+    public void testVerify3(){
+        
+        String[] num = {"95327146", "", ""};
+        VerificaTelefono vt = new VerificaTelefono();
+        assertTrue(vt.verify(num));
+        
+    }
+    
+    @Test
+    public void testVerify4(){
+        
+        String[] num = {"3256", "74", "785641"};
+        VerificaTelefono vt = new VerificaTelefono();
+        assertTrue(vt.verify(num));
+        
+    }
+    
+    @Test
+    public void testVerify5(){
+        
+        String[] num = {"7", "", "64"};
+        VerificaTelefono vt = new VerificaTelefono();
+        assertTrue(vt.verify(num));
+        
+    }
+    
+    @Test
+    public void testVerify6(){
+        
+        String[] num = {"adyhec", "564782", ""};
+        VerificaTelefono vt = new VerificaTelefono();
+        assertFalse(vt.verify(num));
+        
+    }
+    
+    @Test
+    public void testVerify7(){
+        
+        String[] num = {"a.!@_/963", "", ""};
+        VerificaTelefono vt = new VerificaTelefono();
+        assertFalse(vt.verify(num));
+        
+    }
+    
+    @Test
+    public void testVerify8(){
+        
+        String[] num = {"7859", "", ".,*?658"};
+        VerificaTelefono vt = new VerificaTelefono();
+        assertFalse(vt.verify(num));
+        
+    }
+}
