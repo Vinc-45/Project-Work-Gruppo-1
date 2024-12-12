e21c4150857f940e41d79fcc8cf1ec4002952ea9
Antonio Bellofatto <>
2024-12-12 11:39:11 +0100

    Metodo main e Start per inizializzare la rubrica e la parte grafica

--- src/main/java/it/unisa/diem/softeng/gruppo1/Rubrica.java
+++ src/main/java/it/unisa/diem/softeng/gruppo1/Rubrica.java
@@ -1,13 +1,26 @@
-
 package it.unisa.diem.softeng.gruppo1;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
 
 /**
  * @brief Classe che inizializza l'applicazione.
  */
-public class Rubrica {
+public class Rubrica extends Application {
 
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         
+        
+    launch(args);
+   
     }
-    
-}
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        GestioneRubrica rubrica = new GestioneRubrica();
+        InterfacciaUtente view = new InterfacciaUtente(rubrica);
+        view.setStage(primaryStage);
+        Stage stage = view.getStage();
+        stage.setScene(view.getPrimaryScene());
+        stage.show();
+    }
+} 
